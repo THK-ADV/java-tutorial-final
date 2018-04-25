bc6f137d6b96cf0ebceedef3d1a40ad883972118
Florian Herborn <florian.herborn@th-koeln.de>
2018-04-25 14:40:28 +0200

    Refactored parameters

--- src/Person.java
+++ src/Person.java
@@ -14,6 +14,7 @@ public class Person {
         this.adresse = adresse;
     }
 
+    // ----------- Setter und Getter ----------
     public String getVorname() {
         return vorname;
     }
--- src/Raum.java
+++ src/Raum.java
@@ -2,16 +2,16 @@ import java.util.ArrayList;
 
 public class Raum {
     private String nummer, name;
-    private ArrayList<InventarGegenstand> inventarGegenstände;
+    private ArrayList<InventarGegenstand> inventar;
 
     public Raum(String nummer, String name) {
         this(nummer, name, new ArrayList<>());
     }
 
-    public Raum(String nummer, String name, ArrayList<InventarGegenstand> inventarGegenstände) {
+    public Raum(String nummer, String name, ArrayList<InventarGegenstand> inventar) {
         this.nummer = nummer;
         this.name = name;
-        this.inventarGegenstände = inventarGegenstände;
+        this.inventar = inventar;
     }
 
     public String getNummer() {
@@ -30,11 +30,11 @@ public class Raum {
         this.name = name;
     }
 
-    public ArrayList<InventarGegenstand> getInventarGegenstände() {
-        return inventarGegenstände;
+    public ArrayList<InventarGegenstand> getInventar() {
+        return inventar;
     }
 
-    public void setInventarGegenstände(ArrayList<InventarGegenstand> inventarGegenstände) {
-        this.inventarGegenstände = inventarGegenstände;
+    public void setInventar(ArrayList<InventarGegenstand> inventar) {
+        this.inventar = inventar;
     }
 }
