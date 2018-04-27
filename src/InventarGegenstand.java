95b10f911bc4ef2f6b6f5aa45fbe2b3987e6d574
Florian Herborn <florian.herborn@th-koeln.de>
2018-04-27 11:36:00 +0200

    Added inventarGegenstand.isGleicherGegenstand

--- src/InventarGegenstand.java
+++ src/InventarGegenstand.java
@@ -12,4 +12,8 @@ public class InventarGegenstand {
     public void setName(String name) {
         this.name = name;
     }
+
+    public boolean isGleicherGegenstand(InventarGegenstand gegenstand) {
+        return this.name.equals(gegenstand.name);
+    }
 }
