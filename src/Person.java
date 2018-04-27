dccce309542dbf7910f0c17281a86b097b4f6ce7
Florian Herborn <florian.herborn@th-koeln.de>
2018-04-27 11:33:59 +0200

    Changed Person.alter to Person.geburtsDatum, added person.getAlter()

--- src/Person.java
+++ src/Person.java
@@ -1,16 +1,21 @@
+import java.sql.Timestamp;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.Period;
+
 public class Person {
     private String vorname, nachname;
-    private int alter;
+    private LocalDate geburtsDatum;
     private Adresse adresse;
 
-    public Person(String vorname, String nachname, int alter) {
+    public Person(String vorname, String nachname, LocalDate geburtsDatum) {
         this.vorname = vorname;
         this.nachname = nachname;
-        this.alter = alter;
+        this.geburtsDatum = geburtsDatum;
     }
 
-    public Person(String vorname, String nachname, int alter, Adresse adresse) {
-        this(vorname, nachname, alter);
+    public Person(String vorname, String nachname, LocalDate geburtsDatum, Adresse adresse) {
+        this(vorname, nachname, geburtsDatum);
         this.adresse = adresse;
     }
 
@@ -34,12 +39,12 @@ public class Person {
         this.nachname = nachname;
     }
 
-    public int getAlter() {
-        return alter;
+    public LocalDate getGeburtsDatum() {
+        return geburtsDatum;
     }
 
-    public void setAlter(int alter) {
-        this.alter = alter;
+    public void setGeburtsDatum(LocalDate geburtsDatum) {
+        this.geburtsDatum = geburtsDatum;
     }
 
     public Adresse getAdresse() {
@@ -48,5 +53,9 @@ public class Person {
 
     public void setAdresse(Adresse adresse) {
         this.adresse = adresse;
+    }
+
+    public int getAlter() {
+        return Period.between(geburtsDatum, LocalDate.now()).getYears();
     }
 }
